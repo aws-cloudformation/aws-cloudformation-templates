db6c2390fe31cf064f3c194ea676510392a7abe5
root <root@ip-172-31-62-192.ec2.internal>
2018-04-26 20:07:47 +0000

    Copyright headers added

--- aws/solutions/Fetch-AMI-From-Parameter-Store/lambda_function.py
+++ aws/solutions/Fetch-AMI-From-Parameter-Store/lambda_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """CloudFormation Lambda-backed custom resource that fetches an AMI from the Parameter Store based on the parameter name provided."""
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
