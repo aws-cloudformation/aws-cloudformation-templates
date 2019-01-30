3db3cfdb993bcba70415362f190b186b182eb12d
Egidio Caprino <egidio.caprino@socialmediaemotions.com>
2019-01-30 17:47:16 +0100

    fix: start the build project with the right parameters

--- community/codestar/github-flow/lambda/index.js
+++ community/codestar/github-flow/lambda/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const { CodeCommit, CodeBuild } = require('aws-sdk');
 
-const { AWS_REGION, CODE_BUILD_PROJECT_NAME } = process.env;
+const { AWS_REGION, CODEBUILD_PROJECT_NAME } = process.env;
 
 const pullRequestIdRegExp = /pull-request\/([0-9]+)/;
 const codeCommit = new CodeCommit({ apiVersion: '2015-04-13', region: AWS_REGION });
@@ -12,16 +12,46 @@ const getPullRequestId = ({ Sns }) => {
   return pullRequestId;
 };
 
-const startBuild = async (commitId) => {
+const startBuild = async (commitId, repositoryName, pullRequestId) => (
   await codeBuild.startBuild({
-    projectName: CODE_BUILD_PROJECT_NAME,
+    projectName: CODEBUILD_PROJECT_NAME,
     sourceVersion: commitId,
-  }).promise();
-};
-
-const isMergeable = ({ pullRequestTargets: [{ destinationCommit, mergeBase }] }) => (
-  destinationCommit === mergeBase
+    environmentVariablesOverride: [
+      {
+        name: 'REPOSITORY_NAME',
+        value: repositoryName,
+        type: 'PLAINTEXT',
+      },
+      {
+        name: 'PULL_REQUEST_ID',
+        value: pullRequestId,
+        type: 'PLAINTEXT',
+      },
+      {
+        name: 'CODEBUILD_PROJECT_NAME',
+        value: CODEBUILD_PROJECT_NAME,
+        type: 'PLAINTEXT',
+      },
+    ],
+  }).promise()
 );
+
+const isMergeable = async (pullRequest) => {
+  const { pullRequestTargets: [{ repositoryName, sourceCommit, destinationCommit }] } = pullRequest;
+
+  const { mergeable } = await codeCommit.getMergeConflicts({
+    repositoryName,
+    sourceCommitSpecifier: sourceCommit,
+    destinationCommitSpecifier: destinationCommit,
+    mergeOption: 'FAST_FORWARD_MERGE',
+  }).promise();
+
+  if (!mergeable) {
+    console.log('Pull Request is not mergeable', JSON.stringify(pullRequest));
+  }
+
+  return mergeable;
+};
 
 exports.handler = async (event, context) => {
   console.log('event', JSON.stringify(event));
@@ -32,13 +62,24 @@ exports.handler = async (event, context) => {
     .map(getPullRequestId)
     .map(pullRequestId => codeCommit.getPullRequest({ pullRequestId }).promise())
   );
+
+  console.log('Pull Requests', JSON.stringify(pullRequests));
   
-  await Promise.all(
+  const buildRuns = await Promise.all(
     pullRequests
       .map(({ pullRequest }) => pullRequest)
       .filter(({ pullRequestStatus }) => pullRequestStatus === 'OPEN')
-      .filter(isMergeable)
-      .map(({ pullRequestTargets }) => pullRequestTargets[0].sourceCommit)
-      .map(startBuild)
+      .map(async (pullRequest) => {
+        if (!await isMergeable(pullRequest)) {
+          return false;
+        }
+        const {
+          pullRequestId,
+          pullRequestTargets: [{ repositoryName, sourceCommit }],
+        } = pullRequest;
+        return startBuild(sourceCommit, repositoryName, pullRequestId);
+      })
   );
+
+  console.log('Build Runs', JSON.stringify(buildRuns.filter(Boolean)));
 };
