d5c1392becb04a62e6e1a6a4f0a48cd083ef4065
moto <j3tm0t0@gmail.com>
2020-03-23 18:14:17 +0900

    apply pylint

--- aws/solutions/lambda-backed-cloudformation-custom-resources/get_vpc_main_route_table_id/Routetable.py
+++ aws/solutions/lambda-backed-cloudformation-custom-resources/get_vpc_main_route_table_id/Routetable.py
@@ -10,24 +10,25 @@ FAILED = "FAILED"
 print('Loading function')
 ec2 = boto3.client('ec2')
 
+
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
-    responseData={}
+    responseData = {}
     try:
         if event['RequestType'] == 'Delete':
-            print("Request Type:",event['RequestType'])
+            print("Request Type:", event['RequestType'])
             print("Delete Request - No Physical resources to delete")
         elif event['RequestType'] == 'Create':
-            print("Request Type:",event['RequestType'])
-            VPCID=event['ResourceProperties']['VPCID']
-            RouteTableID=get_vpc(VPCID)
-            responseData={'RoutetableID':RouteTableID}
+            print("Request Type:", event['RequestType'])
+            VPCID = event['ResourceProperties']['VPCID']
+            RouteTableID = get_vpc(VPCID)
+            responseData = {'RoutetableID': RouteTableID}
             print("Sending response to custom resource")
         elif event['RequestType'] == 'Update':
-            print("Request Type:",event['RequestType'])
-            VPCID=event['ResourceProperties']['VPCID']
-            RouteTableID=get_vpc(VPCID)
-            responseData={'RoutetableID':RouteTableID}
+            print("Request Type:", event['RequestType'])
+            VPCID = event['ResourceProperties']['VPCID']
+            RouteTableID = get_vpc(VPCID)
+            responseData = {'RoutetableID': RouteTableID}
             print("Sending response to custom resource")
         responseStatus = 'SUCCESS'
     except Exception as e:
@@ -36,23 +37,25 @@ def lambda_handler(event, context):
         responseData = {'Failure': 'Something bad happened.'}
     send(event, context, responseStatus, responseData)
 
+
 def get_vpc(VPCID):
-    response = ec2.describe_route_tables (
-      Filters=[
-        {
-          'Name': 'association.main',
-          'Values': [ 'true' ]
-        },
-        {
-          'Name': 'vpc-id',
-          'Values': [ VPCID ]
-        }
-      ]
+    response = ec2.describe_route_tables(
+        Filters=[
+            {
+                'Name': 'association.main',
+                'Values': ['true']
+            },
+            {
+                'Name': 'vpc-id',
+                'Values': [VPCID]
+            }
+        ]
     )
     print("Printing the VPC Route Table ID ....")
-    RouteTableID=response['RouteTables'][0]['RouteTableId']
+    RouteTableID = response['RouteTables'][0]['RouteTableId']
     print(RouteTableID)
     return RouteTableID
+
 
 def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
     responseUrl = event['ResponseURL']
@@ -67,8 +70,8 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
     json_responseBody = json.dumps(responseBody)
     print("Response body:\n" + json_responseBody)
     headers = {
-        'content-type' : '',
-        'content-length' : str(len(json_responseBody))
+        'content-type': '',
+        'content-length': str(len(json_responseBody))
     }
     try:
         response = requests.put(responseUrl,
