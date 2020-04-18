6c88104f755614b1f63b6bee86ebf2f64d77aa1f
Dan Johns <danjhd@amazon.com>
2020-04-18 16:22:49 +0100

    convert to python3.8

--- aws/services/CloudFormation/MacrosExamples/Boto3/lambda/macro.py
+++ aws/services/CloudFormation/MacrosExamples/Boto3/lambda/macro.py
@@ -11,42 +11,45 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+import json
 import os
 
-PREFIX = "Boto3::"
+PREFIX = 'Boto3::'
 
-LAMBDA_ARN = os.environ["LAMBDA_ARN"]
+LAMBDA_ARN = os.environ['LAMBDA_ARN']
 
 def handle_template(request_id, template):
-    for name, resource in template.get("Resources", {}).items():
-        if resource["Type"].startswith(PREFIX):
+    for name, resource in template.get('Resources', {}).items():
+        if resource['Type'].startswith(PREFIX):
             resource.update({
-                "Type": "Custom::Boto3",
-                "Version": "1.0",
-                "Properties": {
-                    "ServiceToken": LAMBDA_ARN,
-                    "Mode": resource.get("Mode", ["Create", "Update"]),
-                    "Action": resource["Type"][len(PREFIX):],
-                    "Properties": resource.get("Properties", {}),
-                },
+                'Type': 'Custom::Boto3',
+                'Version': '1.0',
+                'Properties': {
+                    'ServiceToken': LAMBDA_ARN,
+                    'Mode': resource.get('Mode', ['Create', 'Update']),
+                    'Action': resource['Type'][len(PREFIX):],
+                    'Properties': resource.get('Properties', {})
+                }
             })
 
-            if "Mode" in resource:
-                del resource["Mode"]
+            if 'Mode' in resource:
+                del resource['Mode']
 
     return template
 
 def handler(event, context):
-    fragment = event["fragment"]
-    status = "success"
+    print(json.dumps(event))
+
+    fragment = event['fragment']
+    status = 'success'
 
     try:
-        fragment = handle_template(event["requestId"], event["fragment"])
-    except Exception as e:
-        status = "failure"
+        fragment = handle_template(event['requestId'], event['fragment'])
+    except Exception:
+        status = 'failure'
 
     return {
-        "requestId": event["requestId"],
-        "status": status,
-        "fragment": fragment,
+        'requestId': event['requestId'],
+        'status': status,
+        'fragment': fragment
     }
--- aws/services/CloudFormation/MacrosExamples/Boto3/lambda/resource.py
+++ aws/services/CloudFormation/MacrosExamples/Boto3/lambda/resource.py
@@ -11,70 +11,68 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from urllib2 import build_opener, HTTPHandler, Request
-import base64
+from urllib.request import HTTPHandler, Request, build_opener
 import boto3
-import httplib
 import json
 
 def sendResponse(event, context, status, message):
     body = json.dumps({
-        "Status": status,
-        "Reason": message,
-        "StackId": event['StackId'],
-        "RequestId": event['RequestId'],
-        "LogicalResourceId": event['LogicalResourceId'],
-        "PhysicalResourceId": event["ResourceProperties"]["Action"],
-        "Data": {},
+        'Status': status,
+        'Reason': message,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'PhysicalResourceId': event['ResourceProperties']['Action'],
+        'Data': {}
     })
 
-    request = Request(event['ResponseURL'], data=body)
+    request = Request(event['ResponseURL'], data=str.encode(body))
     request.add_header('Content-Type', '')
     request.add_header('Content-Length', len(body))
     request.get_method = lambda: 'PUT'
 
     opener = build_opener(HTTPHandler)
-    response = opener.open(request)
+    opener.open(request)
 
 def execute(action, properties):
-    action = action.split(".")
+    action = action.split('.')
 
     if len(action) != 2:
-        return "FAILED", "Invalid boto3 call: {}".format(".".join(action))
+        return 'FAILED', f"Invalid boto3 call: {'.'.join(action)}"
 
     client, function = action[0], action[1]
 
     try:
         client = boto3.client(client.lower())
     except Exception as e:
-        return "FAILED", "boto3 error: {}".format(e)
+        return 'FAILED', f'boto3 error: {e}'
 
     try:
         function = getattr(client, function)
     except Exception as e:
-        return "FAILED", "boto3 error: {}".format(e)
+        return 'FAILED', f'boto3 error: {e}'
 
     try:
         function(**properties)
     except Exception as e:
-        return "FAILED", "boto3 error: {}".format(e)
+        return 'FAILED', f'boto3 error: {e}'
 
-    return "SUCCESS", "Completed successfully"
+    return 'SUCCESS', 'Completed successfully'
 
 def handler(event, context):
-    print("Received request:", json.dumps(event, indent=4))
+    print(json.dumps(event))
 
-    request = event["RequestType"]
-    properties = event["ResourceProperties"]
+    request = event['RequestType']
+    properties = event['ResourceProperties']
 
-    if any(prop not in properties for prop in ("Action", "Properties")):
-        print("Bad properties", properties)
-        return sendResponse(event, context, "FAILED", "Missing required parameters")
+    if any(prop not in properties for prop in ('Action', 'Properties')):
+        print('Bad properties', properties)
+        return sendResponse(event, context, 'FAILED', 'Missing required parameters')
 
-    mode = properties["Mode"]
+    mode = properties['Mode']
 
     if request == mode or request in mode:
-        status, message = execute(properties["Action"], properties["Properties"])
+        status, message = execute(properties['Action'], properties['Properties'])
         return sendResponse(event, context, status, message)
 
-    return sendResponse(event, context, "SUCCESS", "No action taken")
+    return sendResponse(event, context, 'SUCCESS', 'No action taken')
