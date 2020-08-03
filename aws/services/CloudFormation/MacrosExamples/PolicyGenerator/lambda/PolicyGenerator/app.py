76553dd4428fe06808764ecd0a1f652fa0e8dc39
Julien Masnada <rostskadat@gmail.com>
2020-08-03 19:27:09 +0200

    Fixed schema and make it more readable

--- aws/services/CloudFormation/MacrosExamples/PolicyGenerator/lambda/PolicyGenerator/app.py
+++ aws/services/CloudFormation/MacrosExamples/PolicyGenerator/lambda/PolicyGenerator/app.py
@@ -1,25 +1,29 @@
-import logging
-import uuid
-import json
+from os.path import join, dirname, abspath
 from samtranslator import policy_templates_data
 from samtranslator.policy_template_processor.processor import PolicyTemplatesProcessor
 from samtranslator.policy_template_processor.exceptions import InsufficientParameterValues, InvalidParameterValues
+import logging
+import uuid
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 # Reference: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-iam-policy.html
 # Use the policy already defined in SAM
+POLICY_TEMPLATES_FILE = join(dirname(abspath(__file__)), "policy_templates.json")
 policy_templates = PolicyTemplatesProcessor._read_json(POLICY_TEMPLATES_FILE)
 processor = PolicyTemplatesProcessor(policy_templates)
-
 
 def generate_policy(policy: dict):
     """This method will convert a Policy Name into a KMS Key policy."""
     policy_name = next(iter(policy))
     policy_parameters = policy[policy_name]
     try:
-        return processor.convert(policy_name, policy_parameters)
+        policy_document = processor.convert(policy_name, policy_parameters)
+        policy_id = str(uuid.uuid4())
+        policy_document['Id'] = f"{policy_name}-{policy_id}"
+        return policy_document
     except Exception:
         raise ValueError(f"Must specify valid parameter values for policy '{policy_name}'")
 
@@ -30,15 +34,20 @@ def handle_template(request_id, template):
         resource_type = resource["Type"]
         if 'Version' in resource.get('Properties', {}).get("KeyPolicy", {}):
             continue
+        properties = resource['Properties']
         if resource_type == "AWS::KMS::Key":
-            properties = resource['Properties']
-            policy = properties["KeyPolicy"]
-            properties["KeyPolicy"] = generate_policy(policy)
-        elif resource_type in ("AWS::SNS::TopicPolicy", "AWS::S3::BucketPolicy"):
-            properties = resource['Properties']
-            policy = properties["PolicyDocument"]
-            properties["PolicyDocument"] = generate_policy(policy)
-
+            policy_key = "KeyPolicy"
+            policy_version = "2012-10-17"
+        elif resource_type in "AWS::S3::BucketPolicy":
+            policy_key = "PolicyDocument"
+            policy_version = "2012-10-17"
+        elif resource_type in "AWS::SNS::TopicPolicy":
+            policy_key = "PolicyDocument"
+            policy_version = "2008-10-17"
+        else:
+            continue
+        properties[policy_key] = generate_policy(properties[policy_key])
+        properties[policy_key]['Version'] = policy_version
     return template
 
 
