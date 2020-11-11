abdf2305d1d1966587de50fe851d0b33321e5655
John Corrales <jjcorrales3@gmail.com>
2020-11-10 17:25:38 -0800

    Update aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
    Co-authored-by: Nikolai Zujev <824109+jaymecd@users.noreply.github.com>

--- aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
+++ aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
@@ -11,7 +11,7 @@ import json
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
-def send(event, context, responseStatus, responseData, reason=None, physicalResourceId=None, noEcho=False):
+def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False, reason=None):
     responseUrl = event['ResponseURL']
 
     print(responseUrl)
