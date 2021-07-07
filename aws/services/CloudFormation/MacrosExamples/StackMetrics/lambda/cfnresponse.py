24a964a30b65798b07dc108ee90d0ba5ce91f2f0
Henri Yandell <477715+hyandell@users.noreply.github.com>
2021-07-06 17:22:26 -0700

    Relicensing cfnresponse.py to Apache-2.0

--- aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
+++ aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
@@ -1,9 +1,5 @@
 #  Copyright 2016 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
-#  This file is licensed to you under the AWS Customer Agreement (the "License").
-#  You may not use this file except in compliance with the License.
-#  A copy of the License is located at http://aws.amazon.com/agreement/ .
-#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
-#  See the License for the specific language governing permissions and limitations under the License.
+#  SPDX-License-Identifier: Apache-2.0
 
 import urllib3
 import json
