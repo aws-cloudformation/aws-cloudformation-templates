70353d980605199cd278cd013ff77bcfffe08093
Gene Wood <gene_wood@cementhorizon.com>
2024-04-06 12:12:46 -0700

    Update cfnresponse code to match documentation
    The cfnresponse code is out of date from what the documentation indicates is the current code.
    This PR updates the code to match the source code described in
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-lambda-function-code-cfnresponsemodule.html#cfn-lambda-function-code-cfnresponsemodule-source

--- aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
+++ aws/services/CloudFormation/MacrosExamples/StackMetrics/lambda/cfnresponse.py
@@ -1,39 +1,47 @@
-#  Copyright 2016 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
-#  SPDX-License-Identifier: Apache-2.0
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 
+from __future__ import print_function
 import urllib3
 import json
-http = urllib3.PoolManager()
+
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
+
+http = urllib3.PoolManager()
+
 
 def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False, reason=None):
     responseUrl = event['ResponseURL']
 
     print(responseUrl)
 
-    responseBody = {}
-    responseBody['Status'] = responseStatus
-    responseBody['Reason'] = reason or 'See the details in CloudWatch Log Stream: ' + context.log_stream_name
-    responseBody['PhysicalResourceId'] = physicalResourceId or context.log_stream_name
-    responseBody['StackId'] = event['StackId']
-    responseBody['RequestId'] = event['RequestId']
-    responseBody['LogicalResourceId'] = event['LogicalResourceId']
-    responseBody['NoEcho'] = noEcho
-    responseBody['Data'] = responseData
+    responseBody = {
+        'Status': responseStatus,
+        'Reason': reason or "See the details in CloudWatch Log Stream: {}".format(context.log_stream_name),
+        'PhysicalResourceId': physicalResourceId or context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'NoEcho': noEcho,
+        'Data': responseData
+    }
 
     json_responseBody = json.dumps(responseBody)
 
-    print("Response body:\n" + json_responseBody)
+    print("Response body:")
+    print(json_responseBody)
 
     headers = {
-        'content-type' : '',
-        'content-length' : str(len(json_responseBody))
+        'content-type': '',
+        'content-length': str(len(json_responseBody))
     }
 
     try:
+        response = http.request('PUT', responseUrl, headers=headers, body=json_responseBody)
+        print("Status code:", response.status)
 
-        response = http.request('PUT',responseUrl,body=json_responseBody.encode('utf-8'),headers=headers)
-        print("Status code: " + response.reason)
+
     except Exception as e:
-        print("send(..) failed executing requests.put(..): " + str(e))
+
+        print("send(..) failed executing http.request(..):", e)
