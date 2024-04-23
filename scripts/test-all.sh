f432ed7b28f95dc629dae12d4dcd218b6059b17e
Eric Beard <ericzbeard@gmail.com>
2024-04-23 15:19:26 -0700

    Change scripts to run from any directory

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -2,12 +2,15 @@
 
 set -eou pipefail
 
-echo "Linting..."
-cfn-lint
+SCRIPT_DIR=$(dirname "$0")
+CONFIG_FILE="${SCRIPT_DIR}/../.cfnlintrc"
+
+echo "Linting with config file ${CONFIG_FILE}"
+cfn-lint --config-file ${CONFIG_FILE} **/*.yaml
 
 echo "Guard..."
 cfn-guard validate --data . \
-    --rules scripts/rules.guard \
+    --rules ${SCRIPT_DIR}/rules.guard \
     --show-summary fail
 
 echo "Success"
