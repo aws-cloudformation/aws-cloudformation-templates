6f726cd528d64529726c2f0b66cf20a9ff581d95
Eric Beard <ericzbeard@gmail.com>
2024-04-23 14:41:15 -0700

    YAML formatting

--- scripts/create-json-all.sh
+++ scripts/create-json-all.sh
@@ -1,6 +1,4 @@
 #!/usr/local/bin/bash
 #
-# Run this script from the cloundformation directory
-#
 ls *.yaml | xargs -n1 ./scripts/create-json-single.sh 
 
---
+++ scripts/format-yaml-all.sh
@@ -0,0 +1,4 @@
+#!/usr/local/bin/bash
+#
+ls *.yaml | xargs -n1 ./scripts/format-yaml-single.sh 
+
