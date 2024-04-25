54c4aa7d443fe20a4946f623ab592c7817174c21
Eric Beard <ericzbeard@gmail.com>
2024-04-25 15:52:10 -0700

    Format yaml aws services

--- scripts/format-yaml-all.sh
+++ scripts/format-yaml-all.sh
@@ -1,5 +1,7 @@
 #!/usr/local/bin/bash
 #
+set -eou pipefail
+
 SCRIPT_DIR=$(dirname "$0")
 find . -name "*.yaml" | xargs -n1 "${SCRIPT_DIR}/format-yaml-single.sh" 
 
--- scripts/format-yaml-single.sh
+++ scripts/format-yaml-single.sh
@@ -1,4 +1,6 @@
 #!/usr/local/bin/bash
+set -eou pipefail
+echo $1
 n=$(basename $1)
 rain fmt $1 > /tmp/$n
 mv /tmp/$n $1
