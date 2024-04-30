78cfa36cb40b829aecf3e7000dcc61f5301f59d4
Eric Beard <ericzbeard@gmail.com>
2024-04-30 09:42:27 -0700

    Fix ExecutionRoleBuilder and remove ExpandStack

--- aws/services/CloudFormation/MacrosExamples/DatetimeNow/README.md
+++ aws/services/CloudFormation/MacrosExamples/DatetimeNow/README.md
@@ -4,12 +4,15 @@ Provides the current datetime as a formatted string.
 
 ## Basic Usage
 
-Place the transform where you would like the output to be placed and provide the string format as the value for the
-`format` Parameter. The macro uses Python so the supported format codes match those used by the python `strftime` method.
-A Full list of supported codes can be found [here](https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes).
+Place the transform where you would like the output to be placed and provide
+the string format as the value for the `format` Parameter. The macro uses
+Python so the supported format codes match those used by the python `strftime`
+method.  A Full list of supported codes can be found
+[here](https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes).
 
-The example below shows retrieving the current date time as a string in the format 'YYYY-MM-DD hh:mm:ss' and setting it as the value
-for a tag on an s3 bucket.
+The example below shows retrieving the current date time as a string in the
+format 'YYYY-MM-DD hh:mm:ss' and setting it as the value for a tag on an s3
+bucket.
 
 ```yaml
 Resources:
--- aws/services/CloudFormation/MacrosExamples/ExecutionRoleBuilder/lambda/policytemplates.py
+++ aws/services/CloudFormation/MacrosExamples/ExecutionRoleBuilder/lambda/policytemplates.py
@@ -1,23 +1,10 @@
-#!/usr/bin/python
-# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
+"This file is used to hold policy snippets."
 
-# This file is used to hold policy snippets.
 # TODO: Move these snippets to managed policies under a hierarchy
 # (e.g. /policytemplates/s3/readonly) and adjust the code accordingly
 
 # Overall role template with substitution tokens for the ROLENAME & ROLETYPE
-roletemplate = '''
+roletemplate = """
 {
    "Type": "AWS::IAM::Role",
    "Properties": {
@@ -35,12 +22,12 @@ roletemplate = '''
       "Policies": [ ]
    }
 }
-'''
+"""
 
 # Policy template for Read Only access to a single S3 bucket
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
 # The substituion token <RESOUCE> will be populated with the actual resource at runtime
-s3_readonly_template = '''
+s3_readonly_template = """
 {
     "PolicyName": "S3-ReadOnly-<UUID>",
     "PolicyDocument" : {
@@ -63,12 +50,12 @@ s3_readonly_template = '''
         }
     ] }
 }
-'''
+"""
 
 # Policy template for Read Write access to a single S3 bucket
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
 # The substituion token <RESOUCE> will be populated with the actual resource at runtime
-s3_readwrite_template = '''
+s3_readwrite_template = """
 {
     "PolicyName": "S3-ReadWrite-<UUID>",
     "PolicyDocument" : {
@@ -91,12 +78,12 @@ s3_readwrite_template = '''
         }
     ] }
 }
-'''
+"""
 
 # Policy template for Read Only access to a single DynamoDB table
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
 # The substituion token <RESOUCE> will be populated with the actual resource at runtime
-ddb_readonly_template = '''
+ddb_readonly_template = """
 {
     "PolicyName": "DDB-ReadOnly-<UUID>",
     "PolicyDocument" : {
@@ -109,12 +96,12 @@ ddb_readonly_template = '''
         }
     ] }
 }
-'''
+"""
 
 # Policy template for Read Write access to a single DynamoDB table
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
 # The substituion token <RESOUCE> will be populated with the actual resource at runtime
-ddb_readwrite_template = '''
+ddb_readwrite_template = """
 {
     "PolicyName": "DDB-ReadWrite-<UUID>",
     "PolicyDocument" : {
@@ -127,12 +114,12 @@ ddb_readwrite_template = '''
         }
     ] }
 }
-'''
+"""
 
 # Policy template for Read Only access to SSM parameter store values under a given path
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
 # The substituion token <RESOUCE> will be populated with the actual resource at runtime
-ssm_readonly_template = '''
+ssm_readonly_template = """
 {
     "PolicyName": "SSM-ReadOnly-<UUID>",
     "PolicyDocument" : {
@@ -150,12 +137,12 @@ ssm_readonly_template = '''
         }
     ] }
 }
-'''
+"""
 
 # Policy template for Read Only access to a single KMS Key
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
 # The substituion token <RESOUCE> will be populated with the actual resource at runtime
-kms_readonly_template = '''
+kms_readonly_template = """
 {
     "PolicyName": "KMS-ReadOnly-<UUID>",
     "PolicyDocument" : {
@@ -168,13 +155,13 @@ kms_readonly_template = '''
         }
     ] }
 }
-'''
+"""
 
 # Policy template that will be added to all roles built by the macro. This eliminates
 # the need to specify permissions that are always desired on each and every role.
 # The policy below provides CloudWatchLogs, CodeDeploy, and SSM Session Manager access.
 # TODO: Customize this policy as desired using allowable AWS IAM syntax.
-all_roles_template = '''
+all_roles_template = """
 {
     "PolicyName": "All-Roles-<UUID>",
     "PolicyDocument" : {
@@ -218,33 +205,33 @@ all_roles_template = '''
         }
     ] }
 }
-'''
+"""
 
 # TODO: Add policy snippets for other services (e.g. SNS, SQS, etc) and other
-# action groups (e.g. ReadOnly, ReadWrite, FullAccess) using the samples above 
+# action groups (e.g. ReadOnly, ReadWrite, FullAccess) using the samples above
 # as a reference
 
 # Simple data structure to hold all of the policy templates for easier lookup/reference
 policytemplates = {}
 # Add the S3 policy templates
 s3policytemplates = {}
-s3policytemplates['ReadOnly'] = s3_readonly_template
-s3policytemplates['ReadWrite'] = s3_readwrite_template
-policytemplates['s3'] = s3policytemplates
+s3policytemplates["ReadOnly"] = s3_readonly_template
+s3policytemplates["ReadWrite"] = s3_readwrite_template
+policytemplates["s3"] = s3policytemplates
 # Add the DynamoDB policy templates
 ddbpolicytemplates = {}
-ddbpolicytemplates['ReadOnly'] = ddb_readonly_template
-ddbpolicytemplates['ReadWrite'] = ddb_readwrite_template
-policytemplates['dynamodb'] = ddbpolicytemplates
+ddbpolicytemplates["ReadOnly"] = ddb_readonly_template
+ddbpolicytemplates["ReadWrite"] = ddb_readwrite_template
+policytemplates["dynamodb"] = ddbpolicytemplates
 # Add the SSM policy templates
 ssmpolicytemplates = {}
-ssmpolicytemplates['ReadOnly'] = ssm_readonly_template
-policytemplates['ssm'] = ssmpolicytemplates
+ssmpolicytemplates["ReadOnly"] = ssm_readonly_template
+policytemplates["ssm"] = ssmpolicytemplates
 # Add the KMS policy templates
 kmspolicytemplates = {}
-kmspolicytemplates['ReadOnly'] = kms_readonly_template
-policytemplates['kms'] = kmspolicytemplates
+kmspolicytemplates["ReadOnly"] = kms_readonly_template
+policytemplates["kms"] = kmspolicytemplates
 # Add the all roles policy templates
 allrolestemplates = {}
-allrolestemplates['default'] = all_roles_template
-policytemplates['allroles'] = allrolestemplates
+allrolestemplates["default"] = all_roles_template
+policytemplates["allroles"] = allrolestemplates
