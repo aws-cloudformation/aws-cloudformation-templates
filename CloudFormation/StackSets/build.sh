e9727f7b1611706ebe17e831387712a2da813b6a
Eric Beard <ericzbeard@gmail.com>
2024-06-26 14:39:44 -0700

    Log setup templates and build script

--- CloudFormation/StackSets/build.sh
+++ CloudFormation/StackSets/build.sh
@@ -13,5 +13,16 @@ rain pkg common-resources-stackset.yaml > common-resources-stackset-pkg.yaml
 echo "Linting common resources stack set..."
 cfn-lint common-resources-stackset-pkg.yaml
 
+echo "Linting log setup for target accounts..."
+cfn-lint log-setup-target-accounts.yaml
+
+echo "Packaging log setup for the management account..."
+rain pkg log-setup-management.yaml > log-setup-management-pkg.yaml
+
+echo "Linting log setup for the management account..."
+cfn-lint log-setup-management-pkg.yaml
+
+
+
 
 
